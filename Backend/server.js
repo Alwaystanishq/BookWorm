6008407562e5b06d3175f9c12a9c632785bd2da6
Tanishq Chauhan <tanishqchauhan03@gmail.com>
2026-04-05 12:39:19 +0530

    Book Routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import authRoutes from "./src/Routes/authRoutes.js";
+import bookRoutes from "./src/Routes/bookRoutes.js";
 import { connectDB } from "./src/lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
