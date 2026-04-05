3e4c606d817141250ab668568f2328ee38e795dd
Tanishq Chauhan <tanishqchauhan03@gmail.com>
2026-04-05 12:11:42 +0530

    Auth Routes

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,14 @@
 import express from "express";
-import "dotenv/config"
-
+import "dotenv/config";
+import authRoutes from "./src/Routes/authRoutes.js";
+import { connectDB } from "./src/lib/db.js";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`)
-})
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
